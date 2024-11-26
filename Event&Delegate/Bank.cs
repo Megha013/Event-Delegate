ea248823d5b4060b3c53560ad2bb4b445d29d76b
Megha013 <meghaawasthi013@gmail.com>
2024-11-26 21:04:49 +0530

    Add project files.

---
+++ Event&Delegate/Bank.cs
@@ -0,0 +1,81 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Event_Delegate
+//{
+//    public delegate void DisplayMessage();
+//    public class Bank
+//    {
+//        public event DisplayMessage LowBal;
+//        public event DisplayMessage ZeroBal;
+//        public event DisplayMessage InsufficientBal;
+//        private double bal;
+//        public Bank(double bal)
+//        {
+//            this.bal = bal;
+//        }
+//        public void Debit(double amt)
+//        {
+//            if (amt > bal)
+//            {
+//                Console.WriteLine("Debit amount : " + amt);
+//                InsufficientBal();
+//            }
+//            else
+//            {
+//                bal -= amt;
+//                Console.WriteLine("Debit amount : " + amt);
+//                Console.WriteLine("Balance : " + bal);
+//                Balance();
+//            }
+//        }
+//        public void Credit(double amt)
+//        {
+//            if (amt > 0)
+//            {
+//                bal += amt;
+//                Console.WriteLine("Credit amount : " + amt);
+//                Console.WriteLine("Balance : " + bal);
+//                Balance();
+//            }
+//        }
+//        public void Balance()
+//        {
+//            if (bal == 0)
+//            {
+//                ZeroBal();
+//            }
+//            else if (bal < 3000)
+//            {
+//                LowBal();
+//            }
+//        }
+//    }
+//    public class Program
+//    {
+//        static void LowBalance()
+//        {
+//            Console.WriteLine("Low balance");
+//        }
+//        static void ZeroBalance()
+//        {
+//            Console.WriteLine("Zero balance");
+//        }
+//        static void InsufficientBalance()
+//        {
+//            Console.WriteLine("Insufficient balance");
+//        }
+//        static void Main(string[] args)
+//        {
+//            Bank b1 = new Bank(10000);
+//            b1.LowBal += new DisplayMessage(LowBalance);
+//            b1.ZeroBal += new DisplayMessage(ZeroBalance);
+//            b1.InsufficientBal += new DisplayMessage(InsufficientBalance);
+//            b1.Credit(12000);
+//            b1.Debit(21000);
+//        }
+//    }
+//}
---
+++ Event&Delegate/Event.cs
@@ -0,0 +1,53 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Event_Delegate
+//{
+//    public class Event
+//    {
+//        public delegate void DisplayMessage();
+//        public class Student
+//        {
+//            // event
+//            public event DisplayMessage Pass;// it is an event name
+//            public event DisplayMessage Fail;
+
+//            public void AcceptMarks(int marks)
+//            {
+//                if (marks >= 40)
+//                {
+//                    Pass();  // raise an event /  call to event
+//                }
+//                else
+//                {
+//                    Fail();
+//                }
+//            }
+//        }
+
+
+//        public class Program
+//        {
+//            static void PassMsg()
+//            {
+//                Console.WriteLine("Congratulations ! You are pass with good score");
+//            }
+//            static void FailMsg()
+//            {
+//                Console.WriteLine("OOPs ! You are Fail");
+//            }
+//            static void Main(string[] args)
+//            {
+//                Student stud = new Student();
+//                // bind event with delegate / configuration
+//                stud.Pass += new DisplayMessage(PassMsg);
+//                stud.Fail += new DisplayMessage(FailMsg);
+
+//                stud.AcceptMarks(30);
+//            }
+//        }
+//    }
+//}
